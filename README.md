db717dfdfacfbfd86ff0ac9f4c51c0b041e27fca
Kaio Braga <146143210+KaioCBraga@users.noreply.github.com>
2025-10-14 22:17:52 +0000

    adiciona scripts`lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 implementando uma copia do https://www.tabnews.com.br para o curso Filipe Deschamps .
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1>I love Maria</h1>
+  return <h1>I love Maria</h1>;
 }
 export default Home;
